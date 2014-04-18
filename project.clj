f315939dc096ec03de499c2cbdaab3aec1aa6c0b
methylene <theairspeedindicator@yahoo.com>
2014-04-18 09:53:44 +0200

    cascalog 2.0.0 has this problem too

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject class-not-found "0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [cascalog "2.1.0"]
+                 [cascalog "2.0.0"]
                  [org.apache.hadoop/hadoop-core "1.1.2"]]
   :main class-not-found.core
   :aot [class-not-found.core])
